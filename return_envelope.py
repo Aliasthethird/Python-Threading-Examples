f9e62e009a8161916544686532a042c7e2dd4fba
Gero Nootz <gero@nootz.us>
2022-02-04 13:21:38 -0600

    refactored

--- return_envelope.py
+++ return_envelope.py
@@ -2,6 +2,21 @@ import time
 import threading
 import queue
 
+class Request():
+    """
+    Containr to pass message to Receiver via return queue
+    """
+    def __init__(self, q: queue.Queue):
+        self.q = q
+
+    def set_request(self, request: str):
+        self.request = request
+
+    def get_request(self) -> str:
+        return self.request
+
+    def put(self, return_value: str):
+        self.q.put(return_value)
 
 class Receiver(threading.Thread):
     """
@@ -9,33 +24,40 @@ class Receiver(threading.Thread):
     The Sender is given the Receiver object  as an input parameter
     """
     def __init__(self):
-        self.in_q = queue.Queue()
+        self._in_q = queue.Queue()
         threading.Thread.__init__(self, daemon=True)
         self.start()
 
     def run(self): 
         i = 0
         while True:  
-            incoming = self.in_q.get()
-            print("new request from", incoming.sender_name, "answered with", i )   
-            incoming.return_q.put(i)
+            incoming = self._in_q.get()
+            print("new request from", incoming.get_request(), "answered with", i )   
+            incoming.put(i)
             i += 1   
+
+    def put(self, return_message) -> None:
+        self._in_q.put(return_message)
+
 
 class Sender(threading.Thread):
     """
     Sends a request to a Receiver object and receives back a number
     indicating that this was the nth request to the Receiver
     """
-    def __init__(self, name: str, receiver: Receiver, target=None):
+    def __init__(self, name: str, receiver: Receiver):
+        
         self.sender_name = name
         self.receiver = receiver
         self.return_q = queue.Queue()
+        self.request = Request(self.return_q)
+        self.request.set_request(self.sender_name)
         threading.Thread.__init__(self, daemon=False)
         self.start()
 
     def run(self): 
         for i in range(3):
-            self.receiver.in_q.put(self)
+            self.receiver.put(self.request)
             print(self.sender_name, 'received', self.return_q.get())
             time.sleep(1)
 
